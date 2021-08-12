4dd82ac0530e0a1f63598e4beece9825e73f65d0
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-08-12 08:44:09 -0300

    Added button to share the page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const btnMobile = document.getElementById('btn-mobile');
 const menuItems = document.getElementById('menu__items');
 const menu = document.getElementById('menu');
+const btnShare = document.getElementById('btn-share');
 
 function toogleMenu(event) {
     if (event.type === 'touchStart') event.preventDefault();
@@ -19,7 +20,22 @@ function toogleItemMenu(event) {
     menu.classList.toggle('active');
 }
 
+async function share() {
+    try {
+        await navigator.share({
+            title: 'Loopstudio',
+            text: 'Discovering new and immersive experiences',
+            url: 'http://projects.shinodalabs.com.br/loopstudios'
+        })
+    } catch (error) {
+        navigator.clipboard.writeText('http://projects.shinodalabs.com.br/loopstudios').then(() => {
+            alert('Link copied to clipboard')
+        })
+    }
+}
+
 btnMobile.addEventListener('click', toogleMenu);
 btnMobile.addEventListener('touchStart', toogleMenu);
 menuItems.addEventListener('click', toogleItemMenu);
 menuItems.addEventListener('touchStart', toogleItemMenu);
+btnShare.addEventListener('click', share);
